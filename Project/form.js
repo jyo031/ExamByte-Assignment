30a7fd019ef933900639a7e940c73a53e3b5f7db
JYOTI TRIPATHI <93487243+jyo031@users.noreply.github.com>
2022-01-30 13:54:57 +0530

    Add files via upload
    Login,Signup , Admin Page

---
+++ Project/form.js
@@ -0,0 +1,23 @@
+
+
+function addForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
+    function displayTable() {
+        document.getElementById("display").style.display = "block";
+      }
+
+      
+
+
+ 
+
+
+  
+
